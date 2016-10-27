42d33a6e5485707a5af680763a6dea76c016f2a8
MaloEUT <malo.rchrd@gmail.com>
2016-10-27 11:42:58 +0200

    event.message.quick_reply

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ app.post('/webhook/', function (req, res) {
       sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
       continue
     }
-    
+    if (event.message.quick_reply) {
+      let text = JSON.stringify(event.message.quick_reply)
+      sendTextMessage(sender, "Postback received: "+text.substring(0, 1000), token)
+      continue
+    }
   }
   res.sendStatus(200)
 })
