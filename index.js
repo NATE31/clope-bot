c142e6df4c46e2b258e598b2639390c23d7c3983
MaloEUT <malo.rchrd@gmail.com>
2016-10-27 17:42:57 +0200

    fail again.

--- index.js
+++ index.js
@@ -27,42 +27,33 @@ app.get('/webhook/', function (req, res) {
 })
 
 // to post data
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-    sendMessage(event.sender.id, {text: "Hi. Send your location"}); // event.message.text
-        }
-  else if (event.message && event.message.attachments && event.message.attachments[0] && event.message.attachments[0].payload && event.message.attachments[0].payload.coordinates) {
-    urlBase = "http://api.wunderground.com/api/57fd25cc02e9da86/conditions/forecast/alert/q/"
-    lat = event.message.attachments[0].payload.coordinates.lat
-    lon = event.message.attachments[0].payload.coordinates.long
-    totUrl = urlBase + String(lat) + "," + String(lon) + ".json"
-
-//                sendMessage(event.sender.id, {text: totUrl});
-
-    request({
-        url: totUrl,
-        json: true
-    }, function (error, response, body) {
-
-        if (!error && response.statusCode === 200) {
-      var rain = body.current_observation.precip_1hr_metric
-      if (rain > 0) {
-        sendMessage(event.sender.id, {text: "It's gonna rain. Grab an umbrella!"});
+app.post('/webhook/', function (req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      if (text === 'Clope') {
+        sendQuickReply(sender)
+        continue
       }
-      else {
-        sendMessage(event.sender.id, {text: "No rain ahead!"});
+      if (text === 'Menu') {
+        sendGenericMessage(sender)
+        continue
       }
-        }
-    })
-  } 
-  events = []
+      sendTextMessage(sender, "😆 Dsl je ne comprend pas " + text.substring(0, 200) + "😆 Tape Menu pour commencer.")
     }
-  req.body.entry[0].messaging = []
-    res.sendStatus(200);
-});
+    if (event.postback) {
+      let text = JSON.stringify(event.postback)
+      sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+      continue
+    }
+    
+  }
+  res.sendStatus(200)
+})
+
 
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.PAGE_ACCESS_TOKEN
@@ -100,7 +91,7 @@ function sendGenericMessage(sender) {
           "image_url": "https://scontent-cdg2-1.xx.fbcdn.net/t31.0-8/14714985_960631460729826_5366735335003603455_o.jpg",
           "buttons": [{
             "type": "web_url",
-            "url": "https://www.localhost.com/?lat=" + lat + "&lon=" + long,
+            "url": "https://www.messenger.com",
             "title": "🚬charger la carte 🚬",
             "webview_height_ratio": "compact"
           }, {
