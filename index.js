aa076ac574fe3f47b6892ab64bf885f9501ad946
MaloEUT <malo.rchrd@gmail.com>
2016-11-22 14:32:50 +0100

    list

--- index.js
+++ index.js
@@ -304,12 +304,10 @@ function sendListRobot(sender) {
        "type": "template",
        "payload": {
            "template_type": "list",
-           "top_element_style": "compact",
+           "top_element_style": "large",
            "elements": [
                {
                    "title": "Classic White T-Shirt",
-                   "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
-                   "subtitle": "100% Cotton, 200% Comfortable",
                    "default_action": {
                        "type": "web_url",
                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
@@ -330,8 +328,6 @@ function sendListRobot(sender) {
                },
                {
                    "title": "Classic Blue T-Shirt",
-                   "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                   "subtitle": "100% Cotton, 200% Comfortable",
                    "default_action": {
                        "type": "web_url",
                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
@@ -352,8 +348,6 @@ function sendListRobot(sender) {
                },
                {
                    "title": "Classic Black T-Shirt",
-                   "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
-                   "subtitle": "100% Cotton, 200% Comfortable",
                    "default_action": {
                        "type": "web_url",
                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
@@ -374,8 +368,6 @@ function sendListRobot(sender) {
                },
                {
                    "title": "Classic Gray T-Shirt",
-                   "image_url": "https://peterssendreceiveapp.ngrok.io/img/gray-t-shirt.png",
-                   "subtitle": "100% Cotton, 200% Comfortable",
                    "default_action": {
                        "type": "web_url",
                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
