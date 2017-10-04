bb56afcad9adfe2c0f2b1ae579810abd9793b4d5
Malo <malo.rchrd+git@gmail.com>
2017-10-04 14:55:41 +0800

    bugfix
    lon not long

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.post('/webhook/', function (req, res) {
         if (!event.message.attachments[0].payload.coordinates) continue;
         let long = event.message.attachments[0].payload.coordinates.long;
         let lat = event.message.attachments[0].payload.coordinates.lat;
-        sendTextMessage(sender, "Merci j'ai bien reçu ta géolocalisation, clic sur le lien pour chargé la carte des Tabacs a proximité https://malorchrd.github.io/first-map/?lat=" + lat +"&long="+ long + "&zoom=15 .")
+        sendTextMessage(sender, "Merci j'ai bien reçu ta géolocalisation, clic sur le lien pour chargé la carte des Tabacs a proximité https://malorchrd.github.io/first-map/?lat=" + lat +"&lon="+ long + "&zoom=15 .")
 
         //console.log('Event.lat : ', JSON.stringify(event.message.attachments[0].payload.coordinates.lat));
         //console.log('Event.long : ', JSON.stringify(event.message.attachments[0].payload.coordinates.long));
